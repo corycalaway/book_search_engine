4225df60a5087b8e99fe307d84360ad7ebec79a9
Cory Calaway <cory.c.calaway@gmail.com>
2021-04-11 12:08:13 -0600

    initial mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,9 +1,75 @@
 // mutations.js:
+import gql from 'graphql-tag';
+
 
 // LOGIN_USER will execute the loginUser mutation set up using Apollo Server.
-
+export const LOGIN_USER = gql`
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      user {
+        username
+      }
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
 // ADD_USER will execute the addUser mutation.
-
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      
+  token
+   user {
+    _id
+    username
+    email
+    bookCount
+    savedBooks {
+      id
+      authors
+      description
+      bookId
+      image
+      link
+      title
+    }
+    
+  }
+  
+      
+    }
+  }
+`;
 // SAVE_BOOK will execute the saveBook mutation.
+export const SAVE_BOOK = gql`
 
+mutation saveBook($input: BookInput!) {
+    saveBook(input: $input) {
+      
+     username
+      savedBooks{
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+     
+      
+    }
+  }
+`;
 // REMOVE_BOOK will execute the removeBook mutation.
+export const REMOVE_BOOK = gql`
+
+mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+   username
+      bookCount
+    }
+  }
+`;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,22 @@
 //queries.js: This will hold the query GET_ME, which will execute the me query set up using Apollo Server.
+import gql from 'graphql-tag';
+
+export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        id
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+    }
+  }
+`;
